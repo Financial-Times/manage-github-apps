51254f480c172aeca32ed883b89eade9678289a0
Simon Plenderleith <simon@simonplend.co.uk>
2018-10-16 17:17:17 +0100

    First version of the tool

--- README.md
+++ README.md
@@ -2,14 +2,27 @@
 
 ## Usage
 
-1. Create a [new GitHub personal access token with all `repo` scopes](https://github.com/settings/tokens/new?description=FT%20Manage%20GitHub%20Apps%20CLI&scopes=repo).
+1. Open up your terminal and in your current working directory drop in the
+   `installations.json` config file that you've been given.
 
-2. Run this command in your terminal:
+2. Create a [new GitHub personal access token with all `repo` scopes](https://github.com/settings/tokens/new?description=FT%20Manage%20GitHub%20Apps%20CLI&scopes=repo).
+
+3. Set these environment variables in your terminal:
 
     ```sh
-    GITHUB_REPO_NAME=$GITHUB_REPO_NAME GITHUB_PERSONAL_ACCESS_TOKEN=$TOKEN npx github:financial-times/manage-github-apps
+    export GITHUB_REPO=$GITHUB_ORG_NAME/$GITHUB_REPO_NAME
+    export GITHUB_PERSONAL_ACCESS_TOKEN=$TOKEN
     ```
 
-    Note: Replace `$GITHUB_REPO_NAME` and `$TOKEN` in the above command with real values.
+    Note: Replace all `$*` instances above with real values.
 
-3. Watch the magic happen... :sparkles:
+4. Run this command:
+
+    ```sh
+    npx github:financial-times/manage-github-apps
+    ```
+
+5. Watch the magic happen... :sparkles:
+
+6. Delete the [GitHub personal access token](https://github.com/settings/tokens)
+   that you created.
---
+++ src/index.js
@@ -0,0 +1,82 @@
+#!/usr/bin/env node
+
+const assert = require('assert');
+const Octokit = require('@octokit/rest');
+
+const octokit = new Octokit({
+	debug: true
+});
+
+const authenticateWithToken = (octokit, token) => {
+
+	octokit.authenticate({
+		type: 'token',
+		token,
+	});
+};
+
+const getAuthenticatedUser = async (octokit) => {
+	const user = await octokit.users.get();
+	
+	return user.data;
+};
+
+const getRepo = async (octokit, { githubRepoOwner, githubRepoName }) => {
+
+	const repo = await octokit.repos.get({
+		owner: githubRepoOwner,
+		repo: githubRepoName,
+	});
+
+	return repo.data;
+};
+
+const main = async () => {
+
+	const githubRepo = process.env.GITHUB_REPO;
+	const [githubRepoOwner, githubRepoName] = githubRepo.split('/');
+	const githubPersonalAccessToken = process.env.GITHUB_PERSONAL_ACCESS_TOKEN;
+
+	const installationsConfigPath = process.cwd() + '/installations.json';
+	console.log(`-- Reading installations config from ${installationsConfigPath}\n`);
+	const { installations } = require(installationsConfigPath);
+
+	authenticateWithToken(octokit, githubPersonalAccessToken);
+
+	const authenticatedUser = await getAuthenticatedUser(octokit);
+	console.log(`✔️ Authenticated as GitHub user ${authenticatedUser.login}`);
+
+	const repo = await getRepo(octokit, { githubRepoOwner, githubRepoName });
+	console.log(`✔️ GitHub repo ${githubRepo} exists\n`);
+
+	const addRequests = installations.map((installation) => {
+		console.log(`➕ Adding repo to installation ${installation.name} (https://github.com/organizations/financial-times-sandbox/settings/installations/${installation.id})`);
+
+		return octokit.apps.addRepoToInstallation({
+			installation_id: installation.id,
+			repository_id: repo.id,
+		});
+	});
+
+	return Promise.all(addRequests).then((responses) => {
+		console.log(`\n➡️ Go to https://github.com/${githubRepo}/settings/installations to see the installed GitHub apps for this repo.`);
+	});
+};
+
+(async () => {
+	try {
+		assert(
+			process.env.GITHUB_REPO,
+			'Environment variable GITHUB_REPO must be set e.g. GitHub-Organization/repo-name'
+		);
+	
+		assert(
+			process.env.GITHUB_PERSONAL_ACCESS_TOKEN,
+			'Environment variable GITHUB_PERSONAL_ACCESS_TOKEN must be set'
+		);
+	
+		await main();
+	} catch (err) {
+		console.error(`ERROR: ${err.message}`);
+	}
+})();
